5226e092faffb59dd7535b19798ae78f3f04007b
Mohamed Khaled <mohamedkhalidmkm0@gmail.com>
2021-10-13 10:18:03 +0200

    Ultrasonic driver added and application file added '

--- Code/distance_calculator/distance_calculator_app.c
+++ Code/distance_calculator/distance_calculator_app.c
@@ -9,18 +9,85 @@
  * Author: Mohamed Khaled.
  *
  */
-
+#include <util/delay.h>
+#include <avr/io.h>
 #include "lcd.h"
+#include "ultrasonic.h"
+/*
+ * Description:
+ * Reduce the error by adding the error value to the measured distance.
+ */
+void calibrateDistance( uint16* distance )
+{
+	if(*distance <= 56)
+	{
+		(*distance)++;
+	}
+	else if (*distance <= 128)
+	{
+		(*distance)++;
+		(*distance)++;
+	}
+	else if (*distance <= 204)
+	{
+		(*distance)++;
+		(*distance)++;
+		(*distance)++;
+	}
+	else if (*distance <= 273)
+	{
+		(*distance)++;
+		(*distance)++;
+		(*distance)++;
+		(*distance)++;
+	}
+	else if (*distance <= 346)
+	{
+		(*distance)++;
+		(*distance)++;
+		(*distance)++;
+		(*distance)++;
+		(*distance)++;
+	}
+	else if(*distance <= 400)
+	{
+		(*distance)++;
+		(*distance)++;
+		(*distance)++;
+		(*distance)++;
+		(*distance)++;
+		(*distance)++;
+	}
+}
 
 int main ( )
 {
+	/* Enable global interrupt */
+	SREG |= (1<<7);
+	/*Initializing LCD and Ultrasonic */
 	LCD_init();
 	LCD_displayString("Distance = ");
-	LCD_moveCursor(0,11);
+	LCD_moveCursor(0,14);
+	LCD_displayString("Cm");
+
+	uint16 distance;
 
 	while(1)
 	{
-
+		Ultrasonic_init();
+		distance = Ultrasonic_readDistance();
+		calibrateDistance(&distance);
+		LCD_moveCursor(0,11);
+		if((distance) < 100)
+		{
+			LCD_intgerToString(distance );
+			LCD_displayCharacter(' ');
+		}
+		else
+		{
+			LCD_intgerToString(distance);
+		}
+		_delay_ms(100);
 	}
 	return 0;
 }
--- Code/distance_calculator/ultrasonic.h
+++ Code/distance_calculator/ultrasonic.h
@@ -23,30 +23,32 @@
 /***********************************************************************
 *                             Definitions                              *
 ***********************************************************************/
+#define ULTRASONIC_TRIGGER_PORT_ID 		PORTB_ID
+#define ULTRASONIC_TRIGGER_PIN_ID		PIN5_ID
 
+
+/*
+ *
+ */
+void Ultrasnoic_edgeProcessing( void );
 
 /***********************************************************************
 *                      Public Functions Prototypes                     *
 ***********************************************************************/
 /*
  * Description:
- * Initialize the ICU driver as required.
+ * Initialize the ICU driver F_ICU = F_CPU/8 and rising edge at first.
  * Setup the ICU call back function.
  * Setup the direction for the trigger pin as output pin through the GPIO driver.
  */
 void Ultrasonic_init( void );
 /*
- *
- */
-void Ultrasonic_Trigger( void );
-/*
- *
+ * Description:
+ * Send the trigger pulse by using Ultrasonic_Trigger function.
+ * Start the measurements by the ICU from this moment
  */
 uint16 Ultrasonic_readDistance( void );
-/*
- *
- */
-void Ultrasnoic_edgeProcessing( void );
+
 
 
 
